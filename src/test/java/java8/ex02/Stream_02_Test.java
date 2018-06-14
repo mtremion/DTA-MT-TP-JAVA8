bcc93f2f9f96badb8b6cae97fa606e3447dc52d9
TREMION Maxime <maxime.tremion@hotmail.fr>
2018-06-14 17:43:42 +0200

    exo 3 finis Stream

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -24,8 +24,7 @@ public class Stream_02_Test {
         List<Order> orders = new Data().getOrders();
 
         // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = null;
-
+        List<Customer> result = orders.stream().map(o -> o.getCustomer()).distinct().collect(Collectors.toList());
         assertThat(result, hasSize(2));
     }
 
@@ -36,8 +35,8 @@ public class Stream_02_Test {
 
         // TODO calculer les statistiques sur les prix des pizzas vendues
         // TODO utiliser l'opération summaryStatistics
-        IntSummaryStatistics result = null;
-
+        IntSummaryStatistics result = orders.stream().flatMap(o -> o.getPizzas().stream()).collect(Collectors.summarizingInt(o->o.getPrice()));
+        System.out.println(result);
 
         assertThat(result.getSum(), is(10900L));
         assertThat(result.getMin(), is(1000));
--- src/test/java/java8/ex03/Stream_03_Test.java
+++ src/test/java/java8/ex03/Stream_03_Test.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import static java.util.stream.Collectors.*;
 
 import static org.hamcrest.Matchers.*;
@@ -26,7 +28,7 @@ public class Stream_03_Test {
         List<Customer> customers = new Data().getCustomers();
 
         // TODO construire une chaîne contenant les prénoms des clients triés et séparé par le caractère "|"
-        String result = null;
+        String result = customers.stream().map(c -> c.getFirstname()).sorted().collect(Collectors.joining("|"));
 
         assertThat(result, is("Alexandra|Cyril|Johnny|Marion|Sophie"));
     }
@@ -37,7 +39,7 @@ public class Stream_03_Test {
         List<Order> orders = new Data().getOrders();
 
         // TODO construire une Map <Client, Commandes effectuées par le client
-        Map<Customer, List<Order>> result = null;
+        Map<Customer, List<Order>> result = orders.stream().collect(Collectors.groupingBy(o -> o.getCustomer()));
 
         assertThat(result.size(), is(2));
         assertThat(result.get(new Customer(1)), hasSize(4));
@@ -61,10 +63,13 @@ public class Stream_03_Test {
     public void test_mapping() throws Exception {
 
         List<Customer> customers = new Data().getCustomers();
+        
+        //Comparator comp = (c1,c2) -> c1.getGender().compare(c2.getGender());
 
         // TODO Construire la map Sexe -> Chaîne représentant les prénoms des clients
-        Map<Gender, String> result = null;
-
+        Map<Gender, String> result = customers.stream().sorted(Comparator.comparing(Customer::getFirstname)).collect(Collectors.toMap(c -> c.getGender(), c -> c.getFirstname(),(s1,s2) -> s1+"|"+s2));
+        System.out.println(result);
+        
         assertThat(result.get(Gender.F), is("Alexandra|Marion|Sophie"));
         assertThat(result.get(Gender.M), is("Cyril|Johnny"));
     }
